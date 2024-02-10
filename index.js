8c47d10b6baca6ebcab42d99ec0c150dd29723fc
herbig <mikeherbig@gmail.com>
2024-02-09 21:57:49 -0500

    bug fixes and full card list

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { Client, Events, GatewayIntentBits, EmbedBuilder } from 'discord.js';
 import removeAccents from 'remove-accents';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFile } from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
-const fs = require('fs');
-const path = require('path');
 
 const client = new Client({
 	intents: [
@@ -16,15 +19,15 @@ const client = new Client({
 // Performs the following modifications:
 // 1. Remove accents, on cards like Maelström
 // 2. Lowercase it
-// 3. Change spaces to underscores
+// 3. Change spaces and dashes to underscores
 // 4. Remove non alpha characters like commas
 //
 // This allows users to query for cards with
 // or without using the accents or apostrophes, commas, etc.
 function normalizeCardName(name) {
     let formatted = removeAccents(name.trim()).toLocaleLowerCase();
-    // Replace spaces with underscores
-    formatted = inputString.replace(/\s+/g, '_');
+    // Replace spaces or dashes with underscores
+    formatted = formatted.replace(/[\s\-]+/g, '_');
     // Remove all non-alphabetic characters except for underscores
     formatted = formatted.replace(/[^a-zA-Z_]/g, '');
     return formatted;
@@ -33,7 +36,7 @@ function normalizeCardName(name) {
 // map of the normalized card name to its display name
 const cardMap = new Map();
 
-fs.readFile(path.join(__dirname, 'card_list.txt'), 
+fs.readFile(path.join(dirname(fileURLToPath(import.meta.url)), 'card_list.txt'), 
     { encoding: 'utf-8' }, (err, data) => {
     if (err) {
         console.error('Error reading the file:', err);
@@ -70,7 +73,7 @@ client.on(Events.MessageCreate, msg => {
                 .setTitle(card)
                 .setDescription(process.env.CURIOSA_URL + normalized)
                 .setImage(process.env.IMG_URL + normalized + '.png')
-                .setColor('#191510'); // TODO do we even need to set this?
+                .setColor('#2B2D31');
             msg.reply({ embeds: [embed] });
         } else {
             msg.reply(`I couldn\'t find \"${match[1]}\", did you mean to say \"Fellbog Frog Men\"?`);
