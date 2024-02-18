b73d4c22738368b7e29052d43f0f41a3dcd8bea1
herbig <mikeherbig@gmail.com>
2024-02-17 21:42:00 -0500

    rulings code

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import { Client, Events, GatewayIntentBits, EmbedBuilder } from 'discord.js';
 import { cardSlug, loadCards, randomizeActivity } from './util.js'
 import { FuzzySearch } from './fuzzysearch.js';
 import dotenv from 'dotenv';
+import { Rulings } from './rulings.js';
 dotenv.config();
 
 const fuzzySearch = new FuzzySearch(await loadCards());
+const rulings = new Rulings();
 
 const client = new Client({
 	intents: [
@@ -31,20 +33,59 @@ client.on(Events.MessageCreate, msg => {
         let embeds = new Array();
 
         matches.forEach((match) => {
-            const query = match.replace('{{', '').replace('}}', '').trim();
 
-            // if query is '{{}}' don't respond to it
-            if (query === '') return;
+            let query = match.replace('{{', '').replace('}}', '').trim();
+            let queryCode = '';
+
+            if (query === '') {
+                // if query is '{{}}' don't respond to it
+                return;
+            } else if (query.startsWith('?')) {
+                query = query.substring(1, query.length);
+                queryCode = '?';
+            }
 
             const cardMatch = fuzzySearch.search(query);
             const embed = new EmbedBuilder();
     
             if (cardMatch !== undefined) {
+
                 const slug = cardSlug(cardMatch);
-                embed.setTitle(cardMatch)
-                    .setDescription(process.env.CURIOSA_URL + slug)
-                    .setImage(process.env.IMG_URL + slug + '.png')
-                    .setColor('#674071');
+                embed.setTitle(`Rulings for ${cardMatch}`)
+                    .setURL(process.env.CURIOSA_URL + slug);
+
+                if (queryCode === '?') {
+                    // if the FAQ scraping breaks, tell them
+                    // to give me a heads up 
+                    if (!rulings.rulingsInitialized()) {
+                        embed.setDescription('Oops, something\'s up with rulings. Please ping @herbig to fix it.')
+                            .setColor('#3F4248');
+                    } else {
+
+                        const faqs = rulings.getRulings(slug);
+                        let description = '';
+
+                        if (faqs === undefined || faqs.length === 0) {
+                            description = `No rulings for ${cardMatch}.`;
+                        } else {
+                            for (let i = 0; i < faqs.length; i++) {
+                                if (i % 2 === 0) {
+                                    description += '**' + faqs[i] + '**\n';
+                                } else {
+                                    description += faqs[i] + '\n\n';
+                                }
+                            }
+                        }
+
+                        embed.setDescription(description)
+                            .setColor('#674071');
+                    }
+                } else {
+                    embed.setTitle(cardMatch)
+                        .setDescription(process.env.CURIOSA_URL + slug)
+                        .setImage(process.env.IMG_URL + slug + '.png')
+                        .setColor('#674071');
+                }
             } else {
                 embed.setDescription(`No card found for \"${query}\"`)
                     .setColor('#3F4248');
---
+++ rulings.js
@@ -0,0 +1,77 @@
+import { By } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
+import { cardSlug } from './util.js';
+
+export class Rulings {
+
+    rulings = new Map()
+
+    constructor() { 
+        // update when the app starts
+        this.loadRulings();
+
+        setInterval(() => {
+            // update again every 2 days
+            this.loadRulings();
+        }, 2 * 24 * 60 * 60 * 1000);
+    }
+
+    async loadRulings() {
+
+        const options = new chrome.Options()
+            .addArguments('--headless')
+            .addArguments('--no-sandbox')
+            .addArguments('--disable-dev-shm-usage');
+
+        const driver = chrome.Driver.createSession(
+            options, 
+            new chrome.ServiceBuilder().build()
+        );
+
+        try {
+            await driver.get('https://curiosa.io/faqs');
+    
+            // give it a while to load, we're not in a rush
+            await driver.sleep(2_000);
+    
+            const updatedRulings = new Map();
+    
+            // find all the elements that contain card names and their FAQs
+            const cards = await driver.findElements(By.css('.max-w-4xl > .pb-6')); 
+    
+            for (const card of cards) {
+    
+                const cardName = await card.findElement(By.css('h3')).getText();
+                const faqs = await card.findElements(By.css('.curiosa-faq'));
+                const faqData = [];
+    
+                for (const faq of faqs) {
+                    const pair = await faq.findElements(By.css('p.mb-4'));
+                    const question = await pair[0].getText();
+                    const answer = await pair[1].getText();
+                    faqData.push(question, answer);
+                }
+    
+                updatedRulings.set(cardSlug(cardName), faqData);
+            }
+    
+            if (updatedRulings.size != 0) {
+                this.rulings = updatedRulings;
+            }
+        } catch (error) {
+            // make sure the map is empty
+            this.rulings = new Map();
+        } finally {
+            await driver.quit();
+        }
+    }
+
+    getRulings(slug) {
+        return this.rulings.get(slug);
+    }
+
+    rulingsInitialized() {
+        return this.rulings.size > 0;
+    }
+}
+
